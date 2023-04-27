3cfcabe34ad2478a3d0003743355d64e900ca8e1
sindystar <hasungah@gmail.com>
2023-04-27 23:24:41 +0900

    js완성

---
+++ skipNavi.js
@@ -0,0 +1,10 @@
+const skipNavi = document.querySelectorAll("#skipNavi li a");
+
+for (let el of skipNavi) {
+  el.addEventListener("focusin", () => {
+    el.classList.add("on");
+  });
+  el.addEventListener("focusout", () => {
+    el.classList.remove("on");
+  })
+}
